094366bba6f207dc6863558c5b30260d3d677e25
Vicky Wong <vickywg998@hotmail.com>
2019-03-31 01:24:19 -0700

    calling weather api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,58 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
 
+
+
+const weatherURL = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/`
+const weatherAPI = '4b66d8d5bc81e8a9dad930ee1de49c57/';
+console.log(weatherAPI)
+
+
+const getLocation = new Promise((resolve, reject) => {
+  if (navigator.geolocation) {
+    console.log('Location aquired');
+    navigator.geolocation.getCurrentPosition(pos => {
+      const coords = {
+        lat: pos.coords.latitude,
+        long: pos.coords.longitude
+      }
+      resolve(coords)
+    })
+  } else {
+    alert('Sorry, you need to enable location services in your browser')
+  }
+})
+
 class App extends Component {
+  state = {
+      loading: true,
+      myLocation: null,
+      weatherData: null
+    };
+  
+
+  componentDidMount() {
+    getLocation
+      .then(response => {
+        console.log(response)
+        this.setState({ myLocation: response });
+      })
+      .then(() => {
+        const { lat, long } = this.state.myLocation;
+        axios.get(
+          `${weatherURL}${weatherAPI}${lat},${long}`
+        ).then(response => {
+          this.setState({
+            weatherData: response.data,
+            loading: false
+          });
+        });
+      });
+  }
+
   render() {
     return (
-      <div>
-        hi
-      </div>
+      <div className="App">{this.state.weatherData}hi</div>
     );
   }
 }
